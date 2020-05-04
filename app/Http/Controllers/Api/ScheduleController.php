5e5272bbed1d2a80673c7f0cff36ade4f915cf7a
Phan Nhat Duy <duyphan2398@gmail.com>
2020-05-04 23:09:40 +0700

    feat: show all schedules of auth user

--- app/Http/Controllers/Api/ScheduleController.php
+++ app/Http/Controllers/Api/ScheduleController.php
@@ -7,13 +7,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\ApiBaseController;
 use App\Models\Schedule;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ScheduleController extends ApiBaseController
 {
-    public function getSchdulesByUserId(User $user){
+    public function getSchdulesByUserId(){
+        $user = Auth::guard('api')->user();
         return response()->json([
             'schedules' => $user->schedules,
             'message'    => 'success'
         ],200);
     }
+
+    public function getSchdulesFilter(Request $request){
+
+    }
+
 }
