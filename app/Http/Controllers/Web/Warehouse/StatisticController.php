1ebfccfec0c5635c6ecc1de36104eaf57ff7bdcb
Phan Nhat Duy <duyphan2398@gmail.com>
2020-06-19 11:41:47 +0700

    ref: ref code in statistic comtroller

--- app/Http/Controllers/Web/Warehouse/StatisticController.php
+++ app/Http/Controllers/Web/Warehouse/StatisticController.php
@@ -5,27 +5,25 @@ namespace App\Http\Controllers\Web\Warehouse;
 
 
 use App\Http\Controllers\WebBaseController;
-use App\Models\Material;
 use App\Models\Receipt;
 use App\Models\Schedule;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Gate;
 
 class StatisticController extends WebBaseController
 {
+    public function __construct()
+    {
+        if (Gate::denies('')) {
+            return response()->json(['error' => 'Not authorized.'], 403);
+        }
+    }
 
     public function index(){
-        if (\Illuminate\Support\Facades\Gate::allows('statistics')){
-            return view('workspace.warehouse.statistic');
-        }
-        return response()->json(['error' => 'Not authorized.'],403);
+        return view('workspace.warehouse.statistic');
     }
 
     public function dataDiagram1(){
-        if (\Illuminate\Support\Facades\Gate::allows('statistics')){
         $data = [];
         $countMonth = Carbon::today()->subMonths(12);
 
@@ -49,12 +47,9 @@ class StatisticController extends WebBaseController
         }
 
         return $data;
-        }
-        return response()->json(['error' => 'Not authorized.'],403);
     }
 
     public function dataDiagram2(){
-        if (\Illuminate\Support\Facades\Gate::allows('statistics')){
         $data = [];
         $total = 0;
         $receipts = Receipt::query()->whereMonth('created_at', '=', Carbon::now()->subMonth()->month)->get();
@@ -82,12 +77,9 @@ class StatisticController extends WebBaseController
             'data'  => $result,
             'total' => $total
         ],200);
-        }
-        return response()->json(['error' => 'Not authorized.'],403);
     }
 
     public function dataDiagram3(){
-        if (\Illuminate\Support\Facades\Gate::allows('statistics')){
         $data = [];
         $countMonth = Carbon::today()->subMonths(12);
 
@@ -108,116 +100,5 @@ class StatisticController extends WebBaseController
             $countMonth->addMonth();
         }
         return $data;
-    }
-    return response()->json(['error' => 'Not authorized.'],403);
-    }
-
-
-    public function dataDiagram4_1(Request $request){
-        $results = collect([]);
-        $total_month = 0;
-        $receipts = Receipt::query()->whereMonth('created_at', '=', Carbon::today()->month)->get();
-
-        foreach (Material::query()->orderBy('name')->get() as $material) {
-            $result = [
-                'id'                        => null,
-                'name'                      => null,
-                'unit'                      => null,
-                'total_last_month'     => 0
-            ];
-            $result['id'] = $material->id;
-            $result['name'] = $material->name;
-            $result['unit'] = $material->unit;
-            foreach ($receipts as $receipt){
-                $products = $receipt->products()->whereHas('materials', function ($query) use ($material){
-                    $query->where('id', $material->id);
-                })->get();
-                if ($products->count() > 0) {
-                    foreach ($products as $product){
-                        $quantity_material = $product->materials()->whereKey($material->id)->first()->pivot->quantity;
-                        $total_month += $quantity_material*$product->pivot->quantity;
-                    }
-                }
-            }
-            $result['total_month'] = $total_month;
-            $results->push($result);
-        }
-        return $this->paginate($results, 10, $request->page);
-        dd($this->paginate($results, 10, 2));
-    }
-
-    public function dataDiagram4_2(Request $request){
-        $results = collect([]);
-        $total_last_month = 0;
-        $receipts = Receipt::query()->whereMonth('created_at', '=', Carbon::today()->subMonth(1)->month)->get();
-
-        foreach (Material::query()->orderBy('name')->get() as $material) {
-            $result = [
-                'id'                        => null,
-                'name'                      => null,
-                'unit'                      => null,
-                'total_last_month'     => 0
-            ];
-            $result['id'] = $material->id;
-            $result['name'] = $material->name;
-            $result['unit'] = $material->unit;
-            foreach ($receipts as $receipt){
-                $products = $receipt->products()->whereHas('materials', function ($query) use ($material){
-                    $query->where('id', $material->id);
-                })->get();
-                if ($products->count() > 0) {
-                    foreach ($products as $product){
-                        $quantity_material = $product->materials()->whereKey($material->id)->first()->pivot->quantity;
-                        $total_last_month += $quantity_material*$product->pivot->quantity;
-                    }
-                }
-            }
-            $result['total_last_month'] = $total_last_month;
-            $results->push($result);
-        }
-        return $this->paginate($results, 10, $request->page);
-    }
-
-    public function dataDiagram4_3(Request $request){
-        $results = collect([]);
-        $total_last_last_month = 0;
-        $receipts = Receipt::query()->whereMonth('created_at', '=', Carbon::today()->subMonth(2)->month)->get();
-
-        foreach (Material::query()->orderBy('name')->get() as $material) {
-            $result = [
-                'id'                        => null,
-                'name'                      => null,
-                'unit'                      => null,
-                'total_last_last_month'     => 0
-            ];
-            $result['id'] = $material->id;
-            $result['name'] = $material->name;
-            $result['unit'] = $material->unit;
-            foreach ($receipts as $receipt){
-                $products = $receipt->products()->whereHas('materials', function ($query) use ($material){
-                    $query->where('id', $material->id);
-                })->get();
-                if ($products->count() > 0) {
-                    foreach ($products as $product){
-                        $quantity_material = $product->materials()->whereKey($material->id)->first()->pivot->quantity;
-                        $total_last_last_month += $quantity_material*$product->pivot->quantity;
-                    }
-                }
-            }
-            $result['total_last_last_month'] = $total_last_last_month;
-            $results->push($result);
-        }
-        return $this->paginate($results, 10, $request->page);
-    }
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    public function paginate($items, $perPage = 5, $page = null, $options = [])
-    {
-        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
-        $items = $items instanceof Collection ? $items : Collection::make($items);
-        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
     }
 }
